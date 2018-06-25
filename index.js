d70e887cfd25817e6d0dbcf2e6bb5a38fe5e24cb
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-06-25 18:49:15 -0300

    Added express session and save msg in database

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const sharedSession = require("express-socket.io-session");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 const Room = require("./models/room");
+const Message = require("./models/message");
 
-app.use(
-  session({
-    secret: "socketio",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 10 * 60 * 1000
-    }
-  })
-);
+const expressSession = session({
+  secret: "socketio",
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 10 * 60 * 1000
+  }
+});
+app.use(expressSession);
+io.use(sharedSession(expressSession, { autoSave: true }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -58,10 +61,23 @@ io.on("connection", socket => {
 
   socket.on("join", roomId => {
     socket.join(roomId);
+    Message.find({ room: roomId }).then(msgs => {
+      socket.emit("msgsList", msgs);
+    });
   });
 
-  socket.on("sendMsg", msg => {
-    console.log(msg);
+  socket.on("sendMsg", data => {
+    const message = new Message({
+      author: socket.handshake.session.user.name,
+      when: new Date(),
+      type: "text",
+      message: data.msg,
+      room: data.room
+    });
+
+    message.save().then(() => {
+      io.to(data.room).emit("newMsg", message);
+    });
   });
 });
 
